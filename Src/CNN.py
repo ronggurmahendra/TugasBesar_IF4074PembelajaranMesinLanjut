d487a4cb8977b8ac877bfbfbdf9aca06cde91664
ronggurmahendra <ronggur.mwp@gmail.com>
2023-09-14 16:11:09 +0700

    initial commit and some template

---
+++ Src/CNN.py
@@ -0,0 +1,17 @@
+class CNN: 
+    layers = []
+
+    def init():
+        self.layers = []
+    
+    def predict():
+        return 0
+    
+    def fit():
+        return 0
+
+    def load_model():
+        return 0
+
+    def save_model():
+        return 0
