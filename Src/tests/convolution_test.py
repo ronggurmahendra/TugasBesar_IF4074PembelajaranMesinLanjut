9108c4f9f11a055ede7ce32a60c3a4dad0e0edac
Averrous Saloom <averrous.saloom@suitmedia.com>
2023-09-16 16:47:49 +0700

    test: add test cases for convolution and complete

---
+++ Src/tests/convolution_test.py
@@ -0,0 +1,21 @@
+from Src.CNN import *
+from matrix_samples import *
+
+def test_standard6x6x1():
+    model = CNN()
+    model.add(ConvolutionLayer(32, [3,3], 0, (0,1)))
+    output = model.predict(standard6x6x1)
+    assert output.shape == (32, 4, 4)
+
+def test_standard6x6x1_with_padding():
+    model = CNN()
+    model.add(ConvolutionLayer(32, [3,3], 1, (0,1)))
+    output = model.predict(standard6x6x1)
+    assert output.shape == (32, 2, 2)
+
+def test_standard6x6x1_with_stride():
+    model = CNN()
+    model.add(ConvolutionLayer(32, [3,3], 0, (0,2)))
+    output = model.predict(standard6x6x1)
+    assert output.shape == (32, 2, 2)
+
