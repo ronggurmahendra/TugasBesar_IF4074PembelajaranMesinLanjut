1186ab2cb031ab6708005cfb6cb786593c1ebced
nandono206 <13520070@std.stei.itb.ac.id>
2023-09-17 20:16:38 +0700

    minor fix complete test

--- Src/main.py
+++ Src/main.py
@@ -2,6 +2,7 @@ from utils import *
 from CNN import *
 
 
+
 images = readDatasets()
 
 # TODO: Change this into loadData for multiple input (for milestone 2)
@@ -10,7 +11,7 @@ preprocess_mat = createMatrix(images, 0)
 
 
 
-print(preprocess_mat)
+print()
 
 model = CNN()
 model.add(ConvolutionLayer(32, [3,3], 0, (0,1)))
